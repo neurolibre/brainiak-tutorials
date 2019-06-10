648dfc013cd1550cdecff3c7427af610148a32a1
ltetrel <loic.tetrel.pro@gmail.com>
2019-06-10 10:58:54 -0400

    adding binder folder

--- examples/factoranalysis/run_mpi_htfa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-mpirun -np 2 python3 latent_factor_from_htfa.py
--- examples/isc/download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget https://ndownloader.figshare.com/files/9342556 -O isc.zip
-unzip -qo isc.zip
-rm -f isc.zip
